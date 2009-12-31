284cf6f89b371524f6e8294817d2656361e7ded6
masao <tmasao@acm.org>
2009-12-31 17:37:49 +0000

    ひととおり動くようになったのでコミット

--- cinii.rb
+++ cinii.rb
@@ -20,35 +20,36 @@ module Zubatto
       open( "#{ base_uri }?q=#{ q }&#{ opts_s }" ) do |res|
          cont = res.read
       end
-      cont
+      #open( "result.xml", "w" ){|io| io.puts cont }
+
+      data = {}
+      parser = LibXML::XML::Parser.string( cont )
+      doc = parser.parse
+      #puts doc.find( "//opensearch:totalResults" )[0].content
+      data[ :q ] = keyword
+      data[ :link ] = doc.find( "//atom:id", "atom:http://www.w3.org/2005/Atom" )[0].content.sub( /&format=atom\b/, "" )
+      data[ :totalResults ] = doc.find( "//opensearch:totalResults" )[0].content.to_i
+      entries = doc.find( "//atom:entry", "atom:http://www.w3.org/2005/Atom" )
+      data[ :entries ] = []
+      entries.each do |e|
+         title = e.find( "./atom:title", "atom:http://www.w3.org/2005/Atom" )[0].content
+         url = e.find( "./atom:id", "atom:http://www.w3.org/2005/Atom" )[0].content
+         author = e.find( ".//atom:author/atom:name", "atom:http://www.w3.org/2005/Atom" ).to_a.map{|name| name.content }.join( "; " )
+         data[ :entries ] << {
+            :title => title,
+            :url => url,
+            :author => author,
+         }
+      end
+      data
    end
 end
 
 if $0 == __FILE__
    puts "Content-Type: text/html\n\n"
-   require "rexml/document"
    include Zubatto
    keyword = ARGV[0] || "information seeking"
-   result = cinii_search( keyword, { :format => "atom" } )
-   #open( "result.xml", "w" ){|io| io.puts result }
-   data = {}
-   parser = LibXML::XML::Parser.string( result )
-   doc = parser.parse
-   #puts doc.find( "//opensearch:totalResults" )[0].content
-   data[ :q ] = keyword
-   data[ :totalResults ] = doc.find( "//opensearch:totalResults" )[0].content
-   entries = doc.find( "//atom:entry", "atom:http://www.w3.org/2005/Atom" )
-   data[ :entries ] = []
-   entries.each do |e|
-      title = e.find( "./atom:title", "atom:http://www.w3.org/2005/Atom" )[0].content
-      url = e.find( "./atom:id", "atom:http://www.w3.org/2005/Atom" )[0].content
-      author = e.find( ".//atom:author/atom:name", "atom:http://www.w3.org/2005/Atom" ).to_a.map{|name| name.content }.join( "; " )
-      data[ :entries ] << {
-         :title => title,
-         :url => url,
-         :author => author,
-      }
-   end
+   data = cinii_search( keyword, { :format => "atom" } )
    rhtml = open("cinii.rhtml"){|io| io.read }
    include ERB::Util
    puts ERB::new( rhtml, $SAFE, 2 ).result( binding )
