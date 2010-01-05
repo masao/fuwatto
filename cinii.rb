fa61ba8059dcdc0a4fe45ecfef94767096b2ed9a
masao <tmasao@acm.org>
2010-01-05 09:34:13 +0000

    publicationName も publisher も空のレコードに対応

--- cinii.rb
+++ cinii.rb
@@ -20,7 +20,8 @@ module Zubatto
          end.join( "&" )
       end
       cont = nil
-      open( "#{ base_uri }?q=#{ q }&#{ opts_s }" ) do |res|
+      opensearch_url = "#{ base_uri }?q=#{ q }&#{ opts_s }"
+      open( opensearch_url ) do |res|
          cont = res.read
       end
       #open( "result.xml", "w" ){|io| io.puts cont }
@@ -29,7 +30,7 @@ module Zubatto
       parser = LibXML::XML::Parser.string( cont )
       doc = parser.parse
       # ref. http://ci.nii.ac.jp/info/ja/if_opensearch.html
-      #p keyword
+      #puts keyword.toeuc
       data[ :q ] = keyword
       data[ :link ] = doc.find( "//atom:id", "atom:http://www.w3.org/2005/Atom" )[0].content.sub( /&format=atom\b/, "" )
       data[ :totalResults ] = doc.find( "//opensearch:totalResults" )[0].content.to_i
@@ -37,11 +38,13 @@ module Zubatto
       data[ :entries ] = []
       entries.each do |e|
          title = e.find( "./atom:title", "atom:http://www.w3.org/2005/Atom" )[0].content
+         #puts title.toeuc
          url = e.find( "./atom:id", "atom:http://www.w3.org/2005/Atom" )[0].content
          author = e.find( ".//atom:author/atom:name", "atom:http://www.w3.org/2005/Atom" ).to_a.map{|name| name.content }.join( "; " )
          pubname = e.find( "./prism:publicationName", "prism:http://prismstandard.org/namespaces/basic/2.0/" )[0]
-         if pubname.nil? 
-            pubname = e.find( "./dc:publisher", "dc:http://purl.org/dc/elements/1.1/" )[0].content
+         if pubname.nil?
+            pubname = e.find( "./dc:publisher", "dc:http://purl.org/dc/elements/1.1/" )[0]
+            pubname = pubname.content if pubname
          else
             pubname = pubname.content
          end
