08a968bbd5dec881f680920fbe6eee343c0ac823
masao <tmasao@acm.org>
2010-01-22 15:11:30 +0000

    changed to use IDF (cinii's hit number as IDF).

--- cinii.rb
+++ cinii.rb
@@ -16,6 +16,12 @@ require "libxml"
 require "MeCab"
 require "extractcontent"
 
+module Math
+   def self::log2( n )
+      Math.log10( n ) / Math.log10( 2 )
+   end
+end
+
 module Fuwatto
    VERSION = '0.1'
    BASE_URI = 'http://kagaku.nims.go.jp/~masao/fuwatto/cinii.rb'
@@ -27,7 +33,7 @@ module Fuwatto
       def initialize( content, mode = "mecab" )
          super()
          @content = NKF.nkf( "-wm0XZ1", content ).gsub( /\s+/, " " ).strip
-         normalized_content = @content.downcase.strip
+         normalized_content = @content.downcase
          clear
          case mode
          when "yahoo"
@@ -124,7 +130,7 @@ module Fuwatto
       keywords.select{|e| not e.nil? and not e.empty? }
    end
 
-   def extract_keywords_mecab( str )
+   def extract_keywords_mecab( str, method = :default )
       mecab = MeCab::Tagger.new( '--node-format=%m\t%H\t%c\n --unk-format=%m\tUNK\t%c\n' )
       lines = mecab.parse( str.toeuc )
       #puts lines
@@ -135,20 +141,27 @@ module Fuwatto
       min = lines.map{|e| e[2].to_i }.min
       lines = lines.map{|e| [ e[0], e[1], e[2].to_i + min.abs + 1 ] } if min < 0
       count = Hash.new( 0 )
+      score = 0
       lines.each_with_index do |line, idx|
          next if line[0] =~ /\A[\x00-\x2f\x3a-\x40\x5b-\x60\x7b-\x7f]+\Z/o # ASCII symbol chars
          next if line[0] =~ /\A(?:w(?:h(?:e(?:re(?:a(?:[st]|fter)|u(?:nto|pon)|in(?:to)?|o[fn]?|from|with|ver|by)?|n(?:(?:so)?ever|ce)?|ther)|o(?:m(?:(?:so)?ever)?|s(?:oever|e)|ever|le)?|i(?:ch(?:(?:so)?ever)?|l(?:st|e)|ther)|at(?:(?:so)?ever)?|y)|i(?:th(?:out|in)?|ll)|e(?:ll|re)?|ould|as)|a(?:l(?:(?:bei|mos)t|on[eg]|though|ready|ways|so|l)|n(?:y(?:(?:wher|on)e|thing|how)?|other|d)?|fter(?:wards)?|bo(?:ut|ve)|gain(?:st)?|mong(?:st)?|r(?:ound|e)|(?:cros)?s|dj|t)?|t(?:h(?:e(?:re(?:(?:upo|i)n|afte|for|by)?|m(?:selves)?|n(?:ce)?|ir|se|y)?|r(?:ough(?:out)?|u)|o(?:ugh|se)|[iu]s|a[nt])|o(?:gether|wards?|o)?)?|s(?:o(?:me(?:t(?:imes?|hing)|(?:wher|on)e|how)?)?|e(?:em(?:ing|ed|s)?|veral)|(?:inc|am)e|h(?:ould|e)|till|uch)?|b(?:e(?:c(?:om(?:es?|ing)|a(?:us|m)e)|fore(?:hand)?|(?:hi|yo)nd|(?:twe)?en|sides?|ing|low)?|oth|ut|y)|h(?:e(?:r(?:e(?:(?:upo|i)n|by)?|s(?:elf)?)?|eafter|nce)?|i(?:m(?:self)?|s)|a(?:[ds]|ve)|ow(?:ever)?)|o(?:u(?:r(?:(?:selve)?s)?|t)|n(?:ce one|ly|to)?|ther(?:wise|s)?|f(?:ten|f)?|(?:ve)?r|wn)|e(?:ve(?:r(?:y(?:(?:wher|on)e|thing)?)?|n)|ls(?:ewher)?e|(?:noug|ac)h|ither|xcept|tc|g)|n(?:o(?:[rw]|t(?:hing)?|body|o?ne)?|e(?:ver(?:theless)?|ither|xt)|amely|where)|m(?:o(?:re(?:over)?|st(?:ly)?)|(?:eanwhil)?e|u(?:ch|st)|y(?:self)?|an?y|ight)|i(?:[efs]|n(?:deed|to|c)?|t(?:s(?:elf)?)?)?|f(?:or(?:mer(?:ly)?)?|urther|irst|rom|ew)|l(?:a(?:tter(?:ly)?|st)|e(?:ast|ss)|td)|y(?:ou(?:r(?:s(?:el(?:ves|f))?)?)?|et)|x(?:author|other |note|subj|cal)|u(?:n(?:der|til)|p(?:on)?|s)|c(?:an(?:not)?|o(?:uld)?)|d(?:uring|own)|per(?:haps)?|v(?:ery|ia)|rather)\Z/o
          #next if line[0].size < 3
          #p line[2]
          #puts line.join("\t")
-         #score = 1 / Math.log( line[2].to_i + 1 )
-         score = Math.log10( line[2].to_i + 10 )
-         #score = line[2].to_i
+         case method
+         when :tf
+            score = 1
+         when :count
+            score = line[2].to_i
+         else
+            score = Math.log2( line[2].to_i + 1 )
+         end
          #pp [ line[0], score, idx ]
-         count[ line[0] ] += score #/ Math.log10( idx + 10 )
+         count[ line[0] ] += score #/ Math.log2( idx + 1 )
+         #count[ line[0] ] += 1
       end
       #pp count
-      ranks = count.keys.sort_by{|e| count[e] }.reverse
+      ranks = count.keys.sort_by{|e| count[e] }.reverse.map{|e| [e,count[e]] }
       #pp ranks
       #3.times do |i|
       #   puts [ i+1, ranks[i], count[ ranks[i] ] ].join( "\t" )
@@ -223,11 +236,16 @@ if $0 == __FILE__
          mode = @cgi.params["mode"][0] || "mecab"
          vector = Document.new( content, mode )
          data = nil
-         vector[0, TERMS].dup.each do |k|
-            if cinii_search( k.toutf8 )[ :totalResults ] < 1
-               vector.delete( k )
-            end
+         vector1 = {}
+         vector.each_with_index do |k, i|
+            res = cinii_search( k[0].toutf8 )
+            next if res[ :totalResults ] < 1
+            score = k[1] * 1 / Math.log2( res[ :totalResults ] + 1 )
+            vector1[ k[0] ] = score
+            break if vector1.size >= 10
          end
+         vector = vector1.keys.sort_by{|k| -vector1[k] }
+         #puts vector1
          keyword = ""
          entries = []
          additional_keywords = []
@@ -235,7 +253,7 @@ if $0 == __FILE__
             keyword = vector[ 0..(TERMS-i-1) ].join( " " ).toutf8
             STDERR.puts keyword
             data = cinii_search( keyword )
-            if data[ :totalResults ].to_i > 0
+            if data[ :totalResults ] > 0
                entries = ( entries + data[ :entries ] ).uniq
                if entries.size < count and entries.size <= count * ( page + 1 ) and vector.size > (TERMS-i)
                   additional_keywords.unshift( vector[ TERMS - i - 1 ].toutf8 )
@@ -243,7 +261,7 @@ if $0 == __FILE__
                   next
                else
                   start = count + 1
-                  while data[ :totalResults ].to_i >= start and entries.size < count * ( page + 1 ) do
+                  while data[ :totalResults ] >= start and entries.size < count * ( page + 1 ) do
                      #p [ entries.size, start ]
                      data = cinii_search( keyword, { :start => start } )
                      entries = ( entries + data[ :entries ] ).uniq
