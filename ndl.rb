2ef498a0923dbe2a411170c18471f1015715e700
masao <tmasao@acm.org>
2010-03-08 20:46:41 +0000

    ndl refactor

--- ndl.rb
+++ ndl.rb
@@ -2,50 +2,9 @@
 # -*- coding: utf-8 -*-
 # $Id$
 
-require "open-uri"
-require "net/http"
-#require "pp"
-require "tempfile"
-require "erb"
-require "cgi"
-require "nkf"
-require "kconv"
-
-require "rubygems"
-require "json"
-require "libxml"
-require "MeCab"
-require "extractcontent"
-
-module Math
-   def self::log2( n )
-      Math.log10( n ) / Math.log10( 2 )
-   end
-end
+require "fuwatto.rb"
 
 module Fuwatto
-   VERSION = '0.1'
-   BASE_URI = 'http://fuwat.to'
-   USER_AGENT = "Fuwatto Search/#{ VERSION }; #{ BASE_URI }"
-
-   # Bag of Words による文書表現
-   class Document < Array
-      attr_reader :content
-      def initialize( content, mode = "mecab" )
-         super()
-         @content = NKF.nkf( "-wm0XZ1", content ).gsub( /\s+/, " " ).strip
-         normalized_content = @content.downcase
-         clear
-         case mode
-         when "yahoo"
-            self.push( *extract_keywords_yahooapi( normalized_content ) )
-         else
-            self.push( *extract_keywords_mecab( normalized_content ) )
-         end
-         #puts self
-      end
-   end
-
    def ndl_search( keyword, opts = {} )
       base_uri = "http://api.porta.ndl.go.jp/servicedp/opensearch"
       q = URI.escape( keyword )
@@ -92,254 +51,27 @@ module Fuwatto
       end
       data
    end
-
-   def cinii_search( keyword, opts = {} )
-      base_uri = "http://ci.nii.ac.jp/opensearch/search"
-      q = URI.escape( keyword )
-      # TODO: Atom/RSSの双方を対象にできるようにすること（現状は Atom のみ）
-      opts[ :format ] = "atom"
-      if not opts.empty?
-         opts_s = opts.keys.map do |e|
-            "#{ e }=#{ URI.escape( opts[e].to_s ) }"
-         end.join( "&" )
-      end
-      cont = nil
-      opensearch_url = "#{ base_uri }?q=#{ q }&#{ opts_s }"
-      open( opensearch_url ) do |res|
-         cont = res.read
-      end
-      #open( "result.xml", "w" ){|io| io.puts cont }
-      data = {}
-      parser = LibXML::XML::Parser.string( cont )
-      doc = parser.parse
-      # ref. http://ci.nii.ac.jp/info/ja/if_opensearch.html
-      #puts keyword.toeuc
-      data[ :q ] = keyword
-      data[ :link ] = doc.find( "//atom:id", "atom:http://www.w3.org/2005/Atom" )[0].content.sub( /&format=atom\b/, "" )
-      data[ :totalResults ] = doc.find( "//opensearch:totalResults" )[0].content.to_i
-      entries = doc.find( "//atom:entry", "atom:http://www.w3.org/2005/Atom" )
-      data[ :entries ] = []
-      entries.each do |e|
-         title = e.find( "./atom:title", "atom:http://www.w3.org/2005/Atom" )[0].content
-         #puts title.toeuc
-         url = e.find( "./atom:id", "atom:http://www.w3.org/2005/Atom" )[0].content
-         author = e.find( ".//atom:author/atom:name", "atom:http://www.w3.org/2005/Atom" ).to_a.map{|name| name.content }.join( "; " )
-         pubname = e.find( "./prism:publicationName", "prism:http://prismstandard.org/namespaces/basic/2.0/" )[0]
-         if pubname.nil?
-            pubname = e.find( "./dc:publisher", "dc:http://purl.org/dc/elements/1.1/" )[0]
-            pubname = pubname.content if pubname
-         else
-            pubname = pubname.content
-         end
-         pubdate = e.find( "./prism:publicationDate", "prism:http://prismstandard.org/namespaces/basic/2.0/" )[0] #.content
-         pubdate = pubdate.nil? ? "" : pubdate.content
-         data[ :entries ] << {
-            :title => title,
-            :url => url,
-            :author => author,
-            :publicationName => pubname,
-            :publicationDate => pubdate,
-         }
-      end
-      data
-   end
-
-   def pdftotext( pdf_str )
-      pdf_file = Tempfile.new( [ "pdf", ".pdf" ] )
-      pdf_file.print pdf_str
-      pdf_file.flush
-      #p pdf_file.size
-      IO.popen( "/usr/local/bin/pdftotext -raw -enc EUC-JP #{ pdf_file.path } -" ) do |io|
-         io.read
-      end
-   end
-
-   YAHOO_KEYWORD_BASEURI = "http://jlp.yahooapis.jp/KeyphraseService/V1/extract"
-   YAHOO_APPID = "W11oHSWxg65mAdRwjBT4ylIdfS9PkHPjVvtJzx9Quwy.um8e1LPf_b.4usSBcmI-"
-   def extract_keywords_yahooapi( str )
-      #cont = open( "?appid=#{ YAHOO_APPID }&sentence=#{ URI.escape( str ) }&output=xml" ){|io| io.read }
-      uri = URI.parse( YAHOO_KEYWORD_BASEURI )
-      response = http_get( uri )
-      http = Net::HTTP.new( uri.host, uri.port )
-      xml = nil
-      http.start do |conn|
-         data = "appid=#{ YAHOO_APPID }&sentence=#{ URI.escape( str.toutf8 ) }&output=xml"
-         # p data
-         res, = conn.post( uri.path, data )
-         xml = res.body
-      end
-      #p xml
-      parser = LibXML::XML::Parser.string( xml )
-      doc = parser.parse
-      keywords = doc.find( "//y:Keyphrase", "y:urn:yahoo:jp:jlp:KeyphraseService" ).map{|e| e.content.toeuc }
-      #keywords.each do |e|
-      #   p e.content
-      #end
-      keywords.select{|e| not e.nil? and not e.empty? }
-   end
-
-   def extract_keywords_mecab( str, method = :default )
-      mecab = MeCab::Tagger.new( '--node-format=%m\t%H\t%c\n --unk-format=%m\tUNK\t%c\n' )
-      lines = mecab.parse( str.toeuc )
-      #puts lines
-      lines = lines.toutf8.split( /\n/ ).map{|l| l.split(/\t/) }
-      lines = lines.select{|l| l[2] and l[1] =~ /^名詞|UNK|形容詞/o and l[1] !~ /接[頭尾]|非自立|代名詞/o }
-      #pp lines
-      raise "Extracting keywords from a text failed." if lines.empty?
-      min = lines.map{|e| e[2].to_i }.min
-      lines = lines.map{|e| [ e[0], e[1], e[2].to_i + min.abs + 1 ] } if min < 0
-      count = Hash.new( 0 )
-      score = 0
-      lines.each_with_index do |line, idx|
-         next if line[0] =~ /\A[\x00-\x2f\x3a-\x40\x5b-\x60\x7b-\x7f]+\Z/o # ASCII symbol chars
-         next if line[0] =~ /\A(?:w(?:h(?:e(?:re(?:a(?:[st]|fter)|u(?:nto|pon)|in(?:to)?|o[fn]?|from|with|ver|by)?|n(?:(?:so)?ever|ce)?|ther)|o(?:m(?:(?:so)?ever)?|s(?:oever|e)|ever|le)?|i(?:ch(?:(?:so)?ever)?|l(?:st|e)|ther)|at(?:(?:so)?ever)?|y)|i(?:th(?:out|in)?|ll)|e(?:ll|re)?|ould|as)|a(?:l(?:(?:bei|mos)t|on[eg]|though|ready|ways|so|l)|n(?:y(?:(?:wher|on)e|thing|how)?|other|d)?|fter(?:wards)?|bo(?:ut|ve)|gain(?:st)?|mong(?:st)?|r(?:ound|e)|(?:cros)?s|dj|t)?|t(?:h(?:e(?:re(?:(?:upo|i)n|afte|for|by)?|m(?:selves)?|n(?:ce)?|ir|se|y)?|r(?:ough(?:out)?|u)|o(?:ugh|se)|[iu]s|a[nt])|o(?:gether|wards?|o)?)?|s(?:o(?:me(?:t(?:imes?|hing)|(?:wher|on)e|how)?)?|e(?:em(?:ing|ed|s)?|veral)|(?:inc|am)e|h(?:ould|e)|till|uch)?|b(?:e(?:c(?:om(?:es?|ing)|a(?:us|m)e)|fore(?:hand)?|(?:hi|yo)nd|(?:twe)?en|sides?|ing|low)?|oth|ut|y)|h(?:e(?:r(?:e(?:(?:upo|i)n|by)?|s(?:elf)?)?|eafter|nce)?|i(?:m(?:self)?|s)|a(?:[ds]|ve)|ow(?:ever)?)|o(?:u(?:r(?:(?:selve)?s)?|t)|n(?:ce one|ly|to)?|ther(?:wise|s)?|f(?:ten|f)?|(?:ve)?r|wn)|e(?:ve(?:r(?:y(?:(?:wher|on)e|thing)?)?|n)|ls(?:ewher)?e|(?:noug|ac)h|ither|xcept|tc|g)|n(?:o(?:[rw]|t(?:hing)?|body|o?ne)?|e(?:ver(?:theless)?|ither|xt)|amely|where)|m(?:o(?:re(?:over)?|st(?:ly)?)|(?:eanwhil)?e|u(?:ch|st)|y(?:self)?|an?y|ight)|i(?:[efs]|n(?:deed|to|c)?|t(?:s(?:elf)?)?)?|f(?:or(?:mer(?:ly)?)?|urther|irst|rom|ew)|l(?:a(?:tter(?:ly)?|st)|e(?:ast|ss)|td)|y(?:ou(?:r(?:s(?:el(?:ves|f))?)?)?|et)|x(?:author|other |note|subj|cal)|u(?:n(?:der|til)|p(?:on)?|s)|c(?:an(?:not)?|o(?:uld)?)|d(?:uring|own)|per(?:haps)?|v(?:ery|ia)|rather)\Z/o
-         #next if line[0].size < 3
-         #p line[2]
-         #puts line.join("\t")
-         case method
-         when :tf
-            score = 1
-         when :count
-            score = line[2].to_i
-         else
-            score = Math.log2( line[2].to_i + 1 )
-         end
-         #pp [ line[0], score, idx ]
-         count[ line[0] ] += score #/ Math.log2( idx + 1 )
-         #count[ line[0] ] += 1
-      end
-      #pp count
-      ranks = count.keys.sort_by{|e| count[e] }.reverse.map{|e| [e,count[e]] }
-      #pp ranks
-      #3.times do |i|
-      #   puts [ i+1, ranks[i], count[ ranks[i] ] ].join( "\t" )
-      #end
-      ranks
-   end
-
-   # Supports redirect
-   def http_get( uri, limit = 3 )
-      STDERR.puts uri
-      raise "Too many redirects: #{ uri }" if limit < 0
-      http_proxy = ENV[ "http_proxy" ]
-      proxy, proxy_port = nil
-      if http_proxy
-         proxy_uri = URI.parse( http_proxy )
-         proxy = proxy_uri.host
-         proxy_port = proxy_uri.port
-      end
-      Net::HTTP.Proxy( proxy, proxy_port ).start( uri.host, uri.port ) do |http|
-         response, = http.get( uri.request_uri, { 'User-Agent'=>USER_AGENT } )
-         #if response.code !~ /^2/
-         #   response.each do |k,v|
-         #      p [ k, v ]
-         #   end
-         #end
-         case response
-         when Net::HTTPSuccess
-            response
-         when Net::HTTPRedirection
-            redirect_uri = URI.parse( response['Location'] )
-            STDERR.puts "redirect to #{ redirect_uri } (#{limit})"
-            http_get( uri + redirect_uri, limit - 1 )
-         else
-            response.error!
-         end
+   class NDLApp < BaseApp
+      TERMS = 5
+      def execute
+         super( :ndl_search, TERMS )
       end
    end
 end
 
 if $0 == __FILE__
    # 検索に使用する最大キーワード数
-   TERMS = 5
-   include Fuwatto
-   include ERB::Util
    @cgi = CGI.new
-   ENV[ 'http_proxy' ] = 'http://wwwout.nims.go.jp:8888' if @cgi.host == "kagaku.nims.go.jp"
-   time_pre = Time.new
+   case @cgi.host
+   when "kagaku.nims.go.jp"
+      ENV[ 'http_proxy' ] = 'http://wwwout.nims.go.jp:8888'
+   when "fuwat.to", "kaede.nier.go.jp"
+      ENV[ 'http_proxy' ] = 'http://ifilter2.nier.go.jp:12080/'
+   end
    begin
-      url = @cgi.params["url"][0]
-      content = @cgi.params["text"][0]
-      if ( url.nil? or url.empty? or url == "http://" ) and ( content.nil? or content.empty? )
-      else
-         if url
-            url = URI.parse( url ) 
-            response = http_get( url )
-            content = response.body
-            case response[ "content-type" ]
-            when /^text\/html\b/
-               content = content.toeuc
-               content = ExtractContent::analyse( content ).join( "\n" )
-               #puts content
-            when /^text\//
-               content = content.toeuc
-            when /^application\/pdf\b/
-               content = pdftotext( content ) #.toeuc
-            else
-               raise "Unknown Content-Type: #{ response[ "content-type" ] }"
-            end
-         end
-         count = @cgi.params["count"][0].to_i
-         count = 20 if count < 1
-         page = @cgi.params["page"][0].to_i
-         mode = @cgi.params["mode"][0] || "mecab"
-         format = @cgi.params["format"][0] || "html"
-         vector = Document.new( content, mode )
-         data = nil
-         vector1 = {}
-         vector.each_with_index do |k, i|
-            res = ndl_search( k[0].toutf8 )
-            next if res[ :totalResults ] < 1
-            score = k[1] * 1 / Math.log2( res[ :totalResults ] + 1 )
-            vector1[ k[0] ] = score
-            break if vector1.size >= 10
-         end
-         vector = vector1.keys.sort_by{|k| -vector1[k] }
-         #puts vector1
-         keyword = ""
-         entries = []
-         additional_keywords = []
-         TERMS.times do |i|
-            keyword = vector[ 0..(TERMS-i-1) ].join( " " ).toutf8
-            STDERR.puts keyword
-            data = ndl_search( keyword )
-            if data[ :totalResults ] > 0
-               entries = ( entries + data[ :entries ] ).uniq
-               if entries.size < count and entries.size <= count * ( page + 1 ) and vector.size >= (TERMS-i)
-                  additional_keywords.unshift( vector[ TERMS - i - 1 ].toutf8 )
-                  #p additional_keywords
-                  next
-               else
-                  start = count + 1
-                  while data[ :totalResults ] >= start and entries.size < count * ( page + 1 ) do
-                     #p [ entries.size, start ]
-                     data = ndl_search( keyword, { :start => start } )
-                     entries = ( entries + data[ :entries ] ).uniq
-                     start += count
-                  end
-               end
-               break
-            end
-         end
-         data[ :entries ] = entries
-         data[ :additional_keywords ] = additional_keywords
-         data[ :count ] = count
-         data[ :page ] = page
-         data[ :searchTime ] = "%0.02f" % ( Time.now - time_pre )
-         case format
-         when "html"
-            rhtml = open( "./ndl.rhtml" ){|io| io.read }
-            cinii_result = ERB::new( rhtml, $SAFE, "<>" ).result( binding )
-         when "json"
-            cinii_result = JSON::generate( data )
-         else
-            raise "unknown format specified: #{ format }"
-         end
-      end
-
-      print @cgi.header
-      rhtml = open("./ndl_top.rhtml"){|io| io.read }
-      puts ERB::new( rhtml, $SAFE, "<>" ).result( binding )
+      app = Fuwatto::NDLApp.new( @cgi )
+      data = app.execute
+      app.output( "ndl", data )
    rescue Exception
       if @cgi then
          print @cgi.header( 'status' => CGI::HTTP_STATUS['SERVER_ERROR'], 'type' => 'text/html' )
---
+++ test/ndl.rb
@@ -0,0 +1,51 @@
+#!/usr/bin/env ruby
+# -*- coding: euc-jp -*-
+# $Id$
+
+require 'test/unit'
+require 'ftools'
+
+$:.push File.join( File.dirname( $0 ), ".." )
+require "ndl.rb"
+
+class TestFuwatto < Test::Unit::TestCase
+   include Fuwatto
+   def test_ndl_search
+      result = ndl_search( "keyword" )
+      assert( result )
+      assert( result[:link] )
+      assert( result[:q] )
+      assert( result[:q] == "keyword" )
+      assert( result[:totalResults] > 0 )
+      assert( result[:entries].size > 0 )
+   end
+end
+
+class TestNDL < Test::Unit::TestCase
+   def setup
+      ENV[ "REQUEST_METHOD" ] = "GET"
+      @cgi = CGI.new( nil )
+   end
+   def test_execute
+      @cgi.params["url"] = [ "http://yahoo.co.jp" ]
+      app = Fuwatto::NDLApp.new( @cgi )
+      result = app.execute
+      assert( result )
+      assert( result[ :totalResults ] > 0 )
+      assert( result[ :totalResults ] > 20 )
+   end
+   def test_output
+      @cgi.params["url"] = [ "http://yahoo.co.jp" ]
+      app = Fuwatto::NDLApp.new( @cgi )
+      result = app.execute
+      html = app.output( "ndl", result )
+   end
+   def test_execute2
+      @cgi.params[ "text" ] = [ "»ùÆ¸µÔÂÔ¤ÈÁêÃÌ½ê¤Î±¿±Ä" ]
+      app = Fuwatto::NDLApp.new( @cgi )
+      result = app.execute
+      assert( result )
+      assert( result[ :totalResults ] > 0 )
+      # assert( result[ :totalResults ] > 20 )
+   end
+end
