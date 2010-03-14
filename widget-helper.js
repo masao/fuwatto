d67fd7d5bb6d99850ffc0ce2ffdec0422289d388
masao <tmasao@acm.org>
2010-03-14 12:33:55 +0000

    ようやく動くようになったので、修正

--- widget-helper.js
+++ widget-helper.js
@@ -1,27 +1,29 @@
+// $Id$
+
 var PREVIEW_BASEURL = 'http://fuwat.to/';
 function preview_fuwatto()
 {
-form = document.forms[0];
-var url = "http://www.asahi.com/paper/editorial.html";
-var count = form.count.value;
-var width = form.width.value;
-var height = form.height.value;
-var title = form.title.value;
-//document.clear;
-document.getElementById( 'preview' ).innerHTML = '';
-var div = document.createElement( "div" );
-div.style.cssText = 'width:'+width+'px;height:'+height+'px;border:solid 1px gray;padding:4px;overflow:auto;';
-div.setAttribute( "id", "fuwatto_result" );
-var h3 = document.createElement( "h3" );
-h3.style.cssText = "margin-bottom:0px";
-h3.appendChild( document.createTextNode( title ) );
-div.appendChild( h3 );
+  form = document.forms[0];
+  var url = "http://www.asahi.com/paper/editorial.html";
+  var count = form.count.value;
+  var width = form.width.value;
+  var height = form.height.value;
+  var title = form.title.value;
+  var database = form.database.value;
+  document.getElementById( 'preview' ).innerHTML = '';
+  var div = document.createElement( "div" );
+  div.style.cssText = 'width:'+width+'px;height:'+height+'px;border:solid 1px gray;padding:4px;overflow:auto;';
+  div.setAttribute( "id", "fuwatto_result" );
+  var h3 = document.createElement( "h3" );
+  h3.style.cssText = "margin-bottom:0px";
+  h3.appendChild( document.createTextNode( title ) );
+  div.appendChild( h3 );
 
-var script = document.createElement( "script" );
-script.setAttribute( "src", PREVIEW_BASEURL + '?format=json;url=' + url + ';count=' + count + ';callback=fuwatto_show_result' );
-script.setAttribute( "type", "text/javascript" );
-script.setAttribute( "defer", "defer" );
+  var script = document.createElement( "script" );
+  script.setAttribute( "src", PREVIEW_BASEURL + database + '?format=json;url=' + url + ';count=' + count + ';callback=fuwatto_show_result' );
+  script.setAttribute( "type", "text/javascript" );
+  script.setAttribute( "defer", "defer" );
 
-div.appendChild( script );
-document.getElementById( 'preview' ).appendChild( div );
+  div.appendChild( script );
+  document.getElementById( 'preview' ).appendChild( div );
 }
