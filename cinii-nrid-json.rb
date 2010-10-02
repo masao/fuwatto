7078a0a8f734fe9827da6983d750d59016e47956
masao <tmasao@acm.org>
2010-10-02 12:56:52 +0000

    add

---
+++ cinii-nrid-json.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/env ruby
+# $Id$
+
+require "fuwatto.rb"
+
+include Fuwatto
+
+@cgi = CGI.new
+
+callback = @cgi.params[ "callback" ][ 0 ]
+name = @cgi.params[ "name" ][ 0 ]
+naid = @cgi.params[ "naid" ]
+
+data = cinii_author_nrid_search( name, naid )
+
+print @cgi.header "application/json"
+result = JSON::generate( data )
+if callback and callback =~ /^\w+$/
+   result = "#{ callback }(#{ result })"
+end
+print result
